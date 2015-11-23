549e91d65e7811c86d2d4fc9311ed7d07aa27bf3
Joshua <iankantian@gmail.com>
2015-11-23 14:16:29 -0800

    adding a date tape to reference on page

--- vendri_demos.js
+++ vendri_demos.js
@@ -1,3 +1,11 @@
 /**
  * Created by Joshua on 11/10/15.
  */
+
+/* Bring in the date and assign variables to it */
+var dateObject = new Date();
+var monthArray = [ "January", "February", "March", "April", "May", "June", "July", "August", "September",
+    "October", "November", "December"];
+document.getElementById("day").innerHTML = dateObject.getDate();
+document.getElementById("month").innerHTML = monthArray[dateObject.getMonth()];
+document.getElementById("year").innerHTML = dateObject.getFullYear();
